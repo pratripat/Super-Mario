df8f8def89ef34e4af79515aa728c53cee2f62d1
Pratyush Ram Tripathi <pratyusht2006@gmail.com>
2021-06-29 21:07:57 +0530

    Added background.
    Signed-off-by: Pratyush Ram Tripathi <pratyusht2006@gmail.com>

--- scripts/entities/flagpole.py
+++ scripts/entities/flagpole.py
@@ -45,9 +45,10 @@ class Flagpole:
             self.game.entities.mario.flip(False)
             self.game.entities.mario.set_animation('hold')
 
-            self.game.entities.mario.rect[0] = self.rect[0]-self.game.entities.mario.rect[2]+12
-            self.game.entities.mario.rect[1] += 4
-            self.flag_position[1] += 4
+            if not self.flag_taken_down:
+                self.game.entities.mario.rect[0] = self.rect[0]-self.game.entities.mario.rect[2]+12
+                self.game.entities.mario.rect[1] += 4
+                self.flag_position[1] += 4
 
             if self.game.entities.mario.rect[1] > self.rect[1]+self.rect[3]-rect[3]:
                 self.game.entities.mario.rect[1] = self.rect[1]+self.rect[3]-rect[3]
@@ -58,11 +59,10 @@ class Flagpole:
             pygame.mixer.music.load('data/music/stage_clear.wav')
             pygame.mixer.music.play()
 
-            self.game.entities.mario.rect[1] = self.flag_position[1]
             self.game.entities.mario.velocity[1] = -1
             self.flag_taken_down = True
 
-            self.game.entities.mario.rect[0] = self.rect[0]+self.game.entities.mario.rect[2]-12
+            self.game.entities.mario.rect[0] = self.rect[0]+self.game.entities.mario.rect[2]-24
             self.game.entities.mario.flip(True)
 
     def load_next_level(self):
--- scripts/renderer.py
+++ scripts/renderer.py
@@ -4,14 +4,28 @@ from .funcs import *
 class Renderer:
     def __init__(self, game):
         self.game = game
+        self.background_image = pygame.image.load('data/graphics/images/background.png').convert()
+        self.background_image.set_colorkey((0,0,0))
+        self.rendering_background = False
 
     def refresh(self):
         self.background_color = (0,0,0)
-        if 'background_color' in [entity['id'] for entity in self.game.tilemap.entities]:
+        if self.game.world_type == 'overworld':
             self.background_color = (107, 139, 255)
+            self.rendering_background = True
+            self.offset = self.game.tilemap.left
+
+    def render_background(self):
+        self.game.screen.fill(self.background_color)
+        if self.rendering_background:
+            position = self.game.entities.mario.position[0] - (self.game.entities.mario.position[0] % self.background_image.get_width()) + self.offset
+
+            self.game.screen.blit(self.background_image, (position-self.game.camera.scroll[0], -self.game.camera.scroll[1]))
+            self.game.screen.blit(self.background_image, (position-self.background_image.get_width()-self.game.camera.scroll[0], -self.game.camera.scroll[1]))
+            self.game.screen.blit(self.background_image, (position+self.background_image.get_width()-self.game.camera.scroll[0], -self.game.camera.scroll[1]))
 
     def render(self):
-        self.game.screen.fill(self.background_color)
+        self.render_background()
         self.render_tiles_with_id(['decorations', 'background'])
         self.render_tiles_with_id(['castle', 'ground', 'chain', 'toad', 'princess'])
         self.game.entities.render()
